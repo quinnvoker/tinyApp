5638289d2f945919e102172e2e5512788488326a
Quinn <quinnvoker@gmail.com>
2020-08-04 22:41:42 +0000

    new link creation now redirects to login if user not logged in

--- express_server.js
+++ express_server.js
@@ -40,8 +40,12 @@ app.get('/urls', (req, res) => {
 
 app.get('/urls/new', (req, res) => {
   const user = users[req.cookies.user_id];
-  let templateVars = { user };
-  res.render('urls_new', templateVars);
+  if (!user) {
+    res.redirect('/login');
+  } else {
+    let templateVars = { user };
+    res.render('urls_new', templateVars);
+  }
 });
 
 app.get('/urls/:shortURL', (req, res) => {
