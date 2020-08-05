f45bec9295c80d8e6aa5b7a54ad2c4f3235a559e
Quinn <quinnvoker@gmail.com>
2020-08-05 14:21:45 +0000

    added comments to helper functions

--- helpers.js
+++ helpers.js
@@ -1,9 +1,11 @@
+// returns id string of the user in the database with given email; undefined if no matching user found
 const getUserByEmail = (email, database) => {
   const foundKeys = Object.keys(database)
     .filter((key) => database[key].email === email);
   return foundKeys.length > 0 ? foundKeys[0] : undefined;
 };
 
+// returns a copy of the database, filtered to only contain entry's belonging to the given userId
 const urlsForUser = (id, database) => {
   return Object.keys(database)
     .filter(key => database[key].userId === id)
@@ -13,6 +15,7 @@ const urlsForUser = (id, database) => {
     }, {});
 };
 
+// generates a string of random characters of a given length
 const generateRandomString = (length) => {
   const allValidChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   return Array.from('0'.repeat(length)) // array initialisation for given length idea from https://stackoverflow.com/questions/4852017/how-to-initialize-an-arrays-length-in-javascript
