665b2ac76cf82b373de706c3fbb650f9bef08d35
Quinn <quinnvoker@gmail.com>
2020-08-05 02:05:58 +0000

    added first test

---
+++ test/helpersTest.js
@@ -0,0 +1,24 @@
+const { expect } = require('chai');
+
+const { findUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = findUserByEmail("user@example.com", testUsers);
+    const expectedOutput = "userRandomID";
+    expect(user).to.equal(expectedOutput);
+  });
+});
