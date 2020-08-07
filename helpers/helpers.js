bb2cdb2d7d59de705e505c3335e4d1d70d33ae63
quinnvoker <quinnvoker@gmail.com>
2020-08-07 14:29:31 -0700

    replaced nested reduce statement with map in getAllHits, much more readable

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -63,8 +63,8 @@ const getAllHits = (url, database) => {
   return Object.keys(urlHits)
     .reduce((list, visitor) => {
       // turn each visitor's hit array into an array of visitorId: timestamp pairs
-      const visits = urlHits[visitor]
-        .reduce((visits, current) => visits.concat({ [visitor]: current }), []);
+      const visits = urlHits[visitor].map(x => ({[visitor]: x}));
+      //.reduce((visits, current) => visits.concat({ [visitor]: current }), []);
       return list.concat(visits);
     }, [])
     // sort combined array by timestamp (descending)
